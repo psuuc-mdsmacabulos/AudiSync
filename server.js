3e3188f811412244107825f4d4c47a53f9bbe4c6
psuuc-mdsmacabulos <danemacabulos6@gmail.com>
2025-03-11 16:51:09 +0800

    Updated - 3/11/2025

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/userLogin.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js'; 
 import cartRoutes from './routes/cartRoutes.js';
+import createUser from './routes/createUser.js';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -17,11 +18,12 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.use("/api/", createUser);
+app.use("/api/auth", userRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/sales", salesRoutes);
-app.use("/api/auth", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
